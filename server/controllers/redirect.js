098a48f49bd168b0f7430170eb4960e1bcb23dac
ZakaryaMeddahi <Z.meddahi2001@gmail.com>
2023-09-11 11:27:14 +0200

    change URLs to production

--- server/controllers/redirect.js
+++ server/controllers/redirect.js
@@ -1,13 +1,15 @@
 const Shortcut = require("../models/Shortcut");
 
+const clientDomain = process.env.CLIENT_DOMAIN;
+const serverDomain = process.env.SERVER_DOMAIN;
 
 const redirect = async (req, res) => {
   try {
     const { pathname } = req.params;
     const shortcut = await Shortcut.findOne({ pathname });
     if(!shortcut) {
-      const url = `https://localhost:3000/${pathname} is not valide`;
-      return res.render('not-found', { url });
+      const url = `${serverDomain}${pathname} is not valide`;
+      return res.render('not-found', { url, clientDomain });
     }
     const { url } = shortcut;
     res.redirect(url);
