754c78c2e2a92f89c287fd66cbcd5b326a8418f6
ZakaryaMeddahi <Z.meddahi2001@gmail.com>
2023-09-11 11:28:07 +0200

    change URLs in client

--- client/config.js
+++ client/config.js
@@ -1,3 +1,3 @@
 
-export const urlEndpoint = 'http://localhost:3000/';
-export const clientUrl = 'http://127.0.0.1:5500/client/';
+export const urlEndpoint = 'https://url-shortener-y7no.onrender.com/';
+export const clientUrl = 'https://url-shortener-tau-sooty.vercel.app/';
