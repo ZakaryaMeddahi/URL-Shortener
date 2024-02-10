6dc8e2b7547fd3e9dda64fecfab4f6bd625d16d1
ZakaryaMeddahi <Z.meddahi2001@gmail.com>
2024-02-10 11:28:38 +0100

    Uncomment Production URLs

--- client/config.js
+++ client/config.js
@@ -1,6 +1,6 @@
 
-// export const urlEndpoint = 'https://url-shortener-y7no.onrender.com/';
-// export const clientUrl = 'https://url-shortener-tau-sooty.vercel.app/';
+export const urlEndpoint = 'https://url-shortener-y7no.onrender.com/';
+export const clientUrl = 'https://url-shortener-tau-sooty.vercel.app/';
 
 // export const urlEndpoint = 'http://127.0.0.1:3000/';
 // export const clientUrl = 'http://localhost:5500/client/';
