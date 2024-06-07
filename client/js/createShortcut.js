44dbb7a7cf732c5612129281f0c30e485408f662
ZakaryaMeddahi <Z.meddahi2001@gmail.com>
2024-06-07 18:45:21 +0100

    clear the inputs when new shortcut is created

--- client/js/createShortcut.js
+++ client/js/createShortcut.js
@@ -1,13 +1,13 @@
 import { urlEndpoint } from '../config.js';
-import updatedShortcut from "./updateShortcut.js";
-import deleteShortcut from "./deleteShortcut.js";
-import copyShortcut from "./copyShortcut.js";
+import updatedShortcut from './updateShortcut.js';
+import deleteShortcut from './deleteShortcut.js';
+import copyShortcut from './copyShortcut.js';
 
 const submit = document.getElementById('submit');
 
 const shortcutCard = ({ _id: id, url, pathname }) => {
   const card = document.createElement('div');
-  card.classList.add("shortcut");
+  card.classList.add('shortcut');
   card.id = id;
   card.dataset.url = url;
   card.dataset.pathname = pathname;
@@ -17,44 +17,44 @@ const shortcutCard = ({ _id: id, url, pathname }) => {
     <!-- <i class="fa-regular fa-circle-check"></i> -->
     <i class="fa-regular fa-pen-to-square"></i>
     <i class="fa-solid fa-trash"></i>`;
-    const copy = card.querySelector('.fa-copy');
-    const update = card.querySelector('.fa-pen-to-square');
-    const trash = card.querySelector('.fa-trash');
-    copyShortcut(copy);
-    updatedShortcut(update);
-    deleteShortcut(trash);
+  const copy = card.querySelector('.fa-copy');
+  const update = card.querySelector('.fa-pen-to-square');
+  const trash = card.querySelector('.fa-trash');
+  copyShortcut(copy);
+  updatedShortcut(update);
+  deleteShortcut(trash);
   return card;
-}
+};
 
 submit.onclick = (e) => {
   e.preventDefault();
-  const url = document.getElementById('url').value;
-  const pathname = document.getElementById('pathname').value;
+  const url = document.getElementById('url');
+  const pathname = document.getElementById('pathname');
   const token = localStorage.getItem('token');
 
   fetch(`${urlEndpoint}api/v1/urls`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': `Bearer ${token}`
+      Authorization: `Bearer ${token}`,
     },
-    body : JSON.stringify({ url, pathname })
+    body: JSON.stringify({ url: url.value, pathname: pathname.value }),
   })
-  .then(async response => {
-    if(!response.ok) {
-      const err = await response.json();
-      const errorCard = document.querySelector('.error');
-      errorCard.textContent = err.message;
-      errorCard.style.visibility = 'visible';
-      return;
-    }
-    return response.json();
-  })
-  .then(data => {
-    const shortcutsContainer = document.querySelector('.shortcuts-container');
-    shortcutsContainer.prepend(shortcutCard(data.shortcut));
-    pathname = '';
-    url = '';
-  })
-  .catch(err => console.error(err));
-}
+    .then(async (response) => {
+      if (!response.ok) {
+        const err = await response.json();
+        const errorCard = document.querySelector('.error');
+        errorCard.textContent = err.message;
+        errorCard.style.visibility = 'visible';
+        return;
+      }
+      return response.json();
+    })
+    .then((data) => {
+      const shortcutsContainer = document.querySelector('.shortcuts-container');
+      shortcutsContainer.prepend(shortcutCard(data.shortcut));
+      pathname.value = '';
+      url.value = '';
+    })
+    .catch((err) => console.error(err));
+};
